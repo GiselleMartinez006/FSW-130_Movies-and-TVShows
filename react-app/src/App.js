dd8d1ed728eef60bd429dd274deb8f0d61ddd091
mtabion <melvin.tabion@bryanuniversity.edu>
2020-03-23 10:12:25 -0700

    Setup components, still needs work

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,47 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
+import Movies from "./components/Movies";
+import TVShows from "./components/TVShows.js";
+const Redux = require("redux");
+let moviesReducerModule = require("./Reducers/moviesReducer");
+let tvshowsReducerModule = require("./Reducers/tvshowsReducer");
 
 function App() {
-  return <div className="App"></div>;
+  let combinedReducer = Redux.combineReducers({
+    movies: moviesReducerModule.reducer,
+    tvshows: tvshowsReducerModule.reducer
+  });
+
+  const store = Redux.createStore(combinedReducer);
+
+  store.dispatch({
+    type: "ADD_MOVIE",
+    payload: {
+      title: "Pan's Labyrinth",
+      genre: "Fantasy",
+      rated: "R",
+      "release date": "January, 2007"
+    }
+  });
+
+  store.dispatch({
+    type: "ADD_TVSHOW",
+    payload: {
+      title: "Walking Dead",
+      location: "Senoia, Georgia",
+      genre: "Thriller"
+    }
+  });
+
+  return;
+  <main>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/movies" component={Movie} />
+      <Route path="/tvshow" component={TVShow} />
+    </Switch>
+  </main>;
 }
 
 export default App;
