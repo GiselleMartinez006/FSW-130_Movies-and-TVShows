1fd3b05d72eb90e5cbb8a9475c2c4aeee0d91d8c
GiselleMartinez006 <53924501+GiselleMartinez006@users.noreply.github.com>
2020-03-26 12:11:23 -0400

    responsive fonts

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,44 @@
 import React from "react";
 import "./App.css";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Movies from "./components/movieComponent";
+import TVShows from "./components/tvShowsComponent";
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 
-function App() {
-  return <div className="App"></div>;
+function App(props) {
+  return (
+    <main
+      style={{
+        display: "grid",
+        height: "95vh",
+        gridTemplateRows: "auto 15vh",
+        gridGap: "5vh",
+        overflowY: "auto"
+      }}
+    >
+      {console.log(props)}
+
+      <Switch>
+        <Route exact path="/" exact render={() => <Home />} />
+        <Route
+          exact
+          path="/movies"
+          exact
+          render={() => <Movies movieArray={props.movies} />}
+        />
+        <Route path="/tvshows" component={TVShows} />
+      </Switch>
+      <Footer />
+    </main>
+  );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    movies: state.movies,
+    shows: state.shows
+  };
+};
+export default connect(mapStateToProps)(App);
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import store from "./redux/store";
+import { Provider } from "react-redux";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
